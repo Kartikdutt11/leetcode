e8739268e9221b949409d3d8f4fe3d781aa99bff
Kartikdutt11 <kartikduttmathur@gmail.com>
2026-07-17 18:15:25 +0000

    Sync LeetCode submission - Is Subsequence (java)

---
+++ leetcode/problems/is_subsequence/solution.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isSubsequence(String s, String t) {
+
+        int j = 0;
+
+        for (int i = 0; i < t.length(); i++) {
+
+            if (j < s.length() && s.charAt(j) == t.charAt(i)) {
+                j++;
+            }
+        }
+
+        if (j == s.length()) {
+           return true; 
+        } else {
+            return false;
+        }
+    }
+}
